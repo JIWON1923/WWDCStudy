4fd6f4399bd9e2437c4585adc787fc8807e6c7c4
JIWON1923 <zest1923@gmail.com>
2024-02-16 19:53:13 +0900

    [#1] - ProjectSetting

---
+++ UIKit/NoticeBoard/NoticeBoard/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoticeBoard
+//
+//  Created by 이지원 on 2024/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
